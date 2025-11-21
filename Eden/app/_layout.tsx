7e0895ecbe5a03fac9593641ccdf8eddf12692b5
greeenboi <suvan.gowrishanker.204@gmail.com>
2025-11-22 00:23:43 +0530

    fix: update splash screen background color and enhance theme provider imports

--- Eden/app/_layout.tsx
+++ Eden/app/_layout.tsx
@@ -1,25 +1,29 @@
+import { useColorScheme } from '@/components/useColorScheme';
 import FontAwesome from '@expo/vector-icons/FontAwesome';
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
+import { ThemeProvider } from '@react-navigation/native';
 import { PortalHost } from '@rn-primitives/portal';
 import { useFonts } from 'expo-font';
 import { Stack } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
 import { useEffect } from 'react';
-import { Platform } from 'react-native';
 import 'react-native-reanimated';
-import "../global.css"
+import "../global.css";
 import { DarkThemeCustom, LightTheme } from '../lib/themeprovider.config';
-import { useColorScheme } from '@/components/useColorScheme';
 
 export {
   // Catch any errors thrown by the Layout component.
-  ErrorBoundary,
+  ErrorBoundary
 } from 'expo-router';
 
 export const unstable_settings = {
   // Ensure that reloading on `/modal` keeps a back button present.
   initialRouteName: '(tabs)',
 };
+
+SplashScreen.setOptions({
+  duration: 1000,
+  fade: true,
+});
 
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
